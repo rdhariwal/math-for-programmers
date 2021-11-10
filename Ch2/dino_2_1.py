f608049a588fbdecbbc07d6f63676c0693074d18
Rupinder <rupinderdhariwal@gmail.com>
2021-11-09 19:10:58 -0500

    2.1 exercise complete

--- Ch2/dino_2_1.py
+++ Ch2/dino_2_1.py
@@ -5,7 +5,14 @@ dino_vectors = [(6,4), (3,1), (1,2), (-1,5), (-2,5), (-3,4), (-4,4),
    (-1,-4), (1,-4), (2,-3), (1,-2), (3,-1), (5,1)
 ]
 
+def add(v1, v2):
+      return(v1[0]+v2[0], v1[1]+v2[1])
+
+dino_vector2 = [add((-1.5, -2.5), v) for v in dino_vectors]
+
 draw(
-   Points(*dino_vectors),
-   Polygon(*dino_vectors),
+   Points(*dino_vectors, color=blue),
+   Polygon(*dino_vectors, color=blue),
+   Points(*dino_vector2, color=red),
+   Polygon(*dino_vector2, color=red),
 )
